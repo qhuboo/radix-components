3694642f4d16b52ac7c9c8f00eee6a41d97954ed
Jose Lopez Renteria <joselukas24@gmail.com>
2024-10-10 22:23:22 -0500

    done with the mechanics of the tabs component, now to make it a modal

--- src/components/RadixTabs/RadixTabs.jsx
+++ src/components/RadixTabs/RadixTabs.jsx
@@ -1,16 +1,66 @@
 import * as Tabs from "@radix-ui/react-tabs";
+import styled from "styled-components";
 
 export default function RadixTabs() {
   return (
-    <Tabs.Root>
-      <Tabs.List>
-        <Tabs.Trigger value="tab1">One</Tabs.Trigger>{" "}
-        <Tabs.Trigger value="tab2">Two</Tabs.Trigger>
-        <Tabs.Trigger value="tab3">Three</Tabs.Trigger>
-      </Tabs.List>
-      <Tabs.Content value="tab1">Tab one content</Tabs.Content>
-      <Tabs.Content value="tab2">Tab two content</Tabs.Content>
-      <Tabs.Content value="tab3">Tab three content</Tabs.Content>
-    </Tabs.Root>
+    <Root>
+      <List>
+        <Trigger value="tab1">One</Trigger>
+        <Trigger value="tab2">Two</Trigger>
+        <Trigger value="tab3">Three</Trigger>
+      </List>
+      <Content value="tab1">Tab one content</Content>
+      <Content value="tab2">Tab two content</Content>
+      <Content value="tab3">Tab three content</Content>
+    </Root>
   );
 }
+
+const Root = styled(Tabs.Root)`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  height: 500px;
+  box-shadow: 0 2px 10px black;
+  border-radius: 6px;
+`;
+
+const List = styled(Tabs.List)`
+  flex-shrink: 0;
+  display: flex;
+  border-bottom: 1px solid black;
+`;
+
+const Trigger = styled(Tabs.Trigger)`
+  background-color: white;
+  padding: 0 20px;
+  height: 45px;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  line-height: 1;
+  border: none;
+
+  &:first-child {
+    border-top-left-radius: 6px;
+  }
+
+  &:last-child {
+    border-top-right-radius: 6px;
+  }
+
+  &:hover {
+    color: #baa7ff;
+  }
+
+  &[data-state="active"] {
+    color: #baa7ff;
+    box-shadow: inset 0 -1px 0 0 currentColor, 0 1px 0 0 currentColor;
+  }
+`;
+
+const Content = styled(Tabs.Content)`
+  padding: 20px;
+`;
